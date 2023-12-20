4d33b9dfdbf4922b00c94b8588863d00bc1045ec
Giovani Gonzalez <darkgeat@gmail.com>
2023-12-20 12:55:24 -0600

    Update remote_attachment_codec.dart
    Adding the complete functionality of the codec

--- lib/src/content/remote_attachment_codec.dart
+++ lib/src/content/remote_attachment_codec.dart
@@ -1,9 +1,11 @@
+import 'package:cryptography/cryptography.dart';
+import 'package:web3dart/crypto.dart';
+import 'package:xmtp/src/common/crypto.dart';
+
 import '../../xmtp.dart';
-import 'codec.dart';
 import 'package:xmtp_proto/xmtp_proto.dart' as xmtp;
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
-import 'package:crypto/crypto.dart';
 
 class EncryptedEncodedContent {
   final String contentDigest;
@@ -11,8 +13,8 @@ class EncryptedEncodedContent {
   final Uint8List salt;
   final Uint8List nonce;
   final Uint8List payload;
-  final int contentLength;
-  final String fileName;
+  final int? contentLength;
+  final String? fileName;
 
   EncryptedEncodedContent(this.contentDigest, this.secret, this.salt,
       this.nonce, this.payload, this.contentLength, this.fileName);
@@ -25,21 +27,64 @@ class RemoteAttachment {
   final Uint8List salt;
   final Uint8List nonce;
   final String scheme;
-  final int contentLength;
-  final String fileName;
+  final int? contentLength;
+  final String? fileName;
   final Fetcher fetcher = HttpFetcher();
 
   RemoteAttachment(this.url, this.contentDigest, this.secret, this.salt,
       this.nonce, this.scheme, this.contentLength, this.fileName);
 
-  EncodedContent decryptEncoded(EncryptedEncodedContent encrypted) {
-    var hashPayload = sha256.convert(encrypted.payload);
-    if (hashPayload.toString() != encrypted.contentDigest) {
+  static Future<EncodedContent> decryptEncoded(
+      EncryptedEncodedContent encrypted) async {
+    var hashPayload = sha256(encrypted.payload);
+    if (bytesToHex(hashPayload) != encrypted.contentDigest) {
       throw Exception("content digest does not match");
     }
 
+    var aes = Ciphertext_Aes256gcmHkdfsha256(
+        hkdfSalt: encrypted.salt,
+        gcmNonce: encrypted.nonce,
+        payload: encrypted.payload);
 
-    return EncodedContent.create();
+    var cipherText = xmtp.Ciphertext(aes256GcmHkdfSha256: aes);
+    var decrypted = await decrypt(encrypted.secret, cipherText);
+
+    return EncodedContent.fromBuffer(decrypted);
+  }
+
+  static Future<EncryptedEncodedContent> encodedEncrypted(
+      dynamic content, Codec<dynamic> codec) async {
+    var secret = List<int>.generate(
+        32, (index) => SecureRandom.forTesting().nextUint32());
+    var encodedContent = await codec.encode(content);
+    var cipherText = await encrypt(secret, encodedContent.writeToBuffer());
+    var contentDigest =
+        bytesToHex(sha256(cipherText.aes256GcmHkdfSha256.payload));
+    return EncryptedEncodedContent(
+        contentDigest,
+        Uint8List.fromList(secret),
+        Uint8List.fromList(cipherText.aes256GcmHkdfSha256.hkdfSalt),
+        Uint8List.fromList(cipherText.aes256GcmHkdfSha256.gcmNonce),
+        Uint8List.fromList(cipherText.aes256GcmHkdfSha256.payload),
+        null,
+        null);
+  }
+
+  static RemoteAttachment from(
+      Uri url, EncryptedEncodedContent encryptedEncodedContent) {
+    if (url.scheme != "https") {
+      throw StateError("scheme must be https://");
+    }
+
+    return RemoteAttachment(
+        url,
+        encryptedEncodedContent.contentDigest,
+        encryptedEncodedContent.secret,
+        encryptedEncodedContent.salt,
+        encryptedEncodedContent.nonce,
+        url.scheme,
+        null,
+        null);
   }
 }
 
@@ -68,25 +113,36 @@ class RemoteAttachmentCodec extends Codec<RemoteAttachment> {
   @override
   Future<RemoteAttachment> decode(EncodedContent encoded) async =>
       RemoteAttachment(
-          Uri.parse(encoded.content.toString()),
-          encoded.parameters["contentDigest"] ?? "",
-          Uint8List.fromList((encoded.parameters["secret"] ?? "").codeUnits),
-          Uint8List.fromList((encoded.parameters["salt"] ?? "").codeUnits),
-          Uint8List.fromList((encoded.parameters["nonce"] ?? "").codeUnits),
-          encoded.parameters["scheme"] ?? "",
-          int.parse(encoded.parameters["contentLength"] ?? ""),
-          encoded.parameters["filename"] ?? "",
+        Uri.parse(encoded.content.toString()),
+        encoded.parameters["contentDigest"] ?? "",
+        Uint8List.fromList((encoded.parameters["secret"] ?? "").codeUnits),
+        Uint8List.fromList((encoded.parameters["salt"] ?? "").codeUnits),
+        Uint8List.fromList((encoded.parameters["nonce"] ?? "").codeUnits),
+        encoded.parameters["scheme"] ?? "",
+        int.parse(encoded.parameters["contentLength"] ?? ""),
+        encoded.parameters["filename"] ?? "",
       );
 
   @override
-  Future<xmtp.EncodedContent> encode(RemoteAttachment decoded) {
-    // TODO: implement encode
-    throw UnimplementedError();
+  Future<xmtp.EncodedContent> encode(RemoteAttachment decoded) async {
+    var parameters = {
+      "contentDigest": decoded.contentDigest,
+      "secret": bytesToHex(decoded.secret),
+      "salt": bytesToHex(decoded.salt),
+      "nonce": bytesToHex(decoded.nonce),
+      "scheme": decoded.scheme,
+      "contentLength": decoded.contentLength.toString(),
+      "filename": decoded.fileName ?? "",
+    };
+    return EncodedContent(
+      type: contentTypeRemoteAttachments,
+      content: Uint8List.fromList(decoded.url.toString().codeUnits),
+      parameters: parameters,
+    );
   }
 
   @override
   String? fallback(RemoteAttachment content) {
     return "Can’t display \"${content.fileName}\". This app doesn’t support remote attachments.";
   }
-
 }
